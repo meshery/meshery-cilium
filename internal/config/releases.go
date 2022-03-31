a954321d7cdfc21257b74bf4c54746e88c546c0f
Pranav Singh <pranavsingh02@hotmail.com>
2022-03-31 22:25:59 +0530

    revert updation of release.go file
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- internal/config/releases.go
+++ internal/config/releases.go
@@ -15,13 +15,10 @@
 package config
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"sort"
+	"sync"
 
 	"github.com/layer5io/meshery-adapter-library/adapter"
+	"github.com/layer5io/meshkit/utils/walker"
 )
 
 // Release is used to save the release informations
@@ -40,66 +37,20 @@ type Asset struct {
 	DownloadURL string `json:"browser_download_url,omitempty"`
 }
 
-// getLatestReleaseNames returns the names of the latest releases
-// limited by the "limit" parameter. 
-// It sorts the result lexographically (descending)
-func GetLatestReleaseNames(limit int) ([]adapter.Version, error) {
-	releases, err := getLatestReleases(uint(limit))
-	if err != nil {
-		return []adapter.Version{}, ErrGetLatestReleaseNames(err)
-	}
-
-	result := make([]adapter.Version, limit)
-
-	if err != nil {
-		return []adapter.Version{}, ErrGetLatestReleaseNames(err)
-	}
-
-	for _, release := range releases {
-		versionStr := string(release.TagName)
-		result = append(result, adapter.Version(versionStr))
-	}
-
-	// Sort the result
-	sort.Slice(result, func(i, j int) bool {
-		return result[i] > result[j]
-	})
-
-	if limit > len(result) {
-		limit = len(result)
-	}
-
-	return result[:limit], nil
+func appendThreadSafe(arr *[]string, s string, m *sync.RWMutex) {
+	m.Lock()
+	defer m.Unlock()
+	*arr = append((*arr), s)
 }
 
-// GetLatestReleases fetches the latest releases from the cilium mesh repository
-func getLatestReleases(releases uint) ([]*Release, error) {
-	releaseAPIURL := "https://api.github.com/repos/cilium/cilium/releases?per_page=" + fmt.Sprint(releases)
-	// We need a variable url here hence using nosec
-	// #nosec
-	resp, err := http.Get(releaseAPIURL)
-	if err != nil {
-		return []*Release{}, ErrGetLatestReleases(err)
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return []*Release{}, ErrGetLatestReleases(fmt.Errorf("unexpected status code: %d", resp.StatusCode))
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return []*Release{}, ErrGetLatestReleases(err)
-	}
-
-	var releaseList []*Release
-
-	if err = json.Unmarshal(body, &releaseList); err != nil {
-		return []*Release{}, ErrGetLatestReleases(err)
-	}
-
-	if err = resp.Body.Close(); err != nil {
-		return []*Release{}, ErrGetLatestReleases(err)
-	}
-
-	return releaseList, nil
+// GetFileNames takes the url of a github repo and the path to a directory. Then returns all the filenames from that directory
+func GetFileNames(owner string, repo string, path string) ([]string, error) {
+	g := walker.NewGit()
+	var fs []string
+	var m sync.RWMutex
+	err := g.Owner(owner).Repo(repo).Branch("master").Root(path).RegisterFileInterceptor(func(f walker.File) error {
+		appendThreadSafe(&fs, f.Name, &m)
+		return nil
+	}).Walk()
+	return fs, err
 }
